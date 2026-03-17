8870a4c0058e079b20163b3e9d0665608c9c7ac8
Asturian <95526997+Raying-9@users.noreply.github.com>
2026-03-17 07:59:07 +0000

    Fix 7.2B server setup on g5

--- run_rwkv_7_2b_server.sh
+++ run_rwkv_7_2b_server.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd /home/asturian/rwkv-rs-vanilla
-source /home/asturian/use_cuda_12_8.sh >/dev/null
+export HOME=/public/home/ssjxusr
+export CUDA_HOME=/usr/local/cuda-12.8
+export CUDA_PATH=/usr/local/cuda-12.8
+export CUDARC_CUDA_VERSION=12080
+export PATH=/public/home/ssjxusr/.cargo/bin:/usr/local/cuda-12.8/bin:/usr/local/bin:/usr/bin:/bin
+export LD_LIBRARY_PATH=/usr/local/cuda-12.8/targets/x86_64-linux/lib:/usr/local/cuda-12.8/nvvm/lib64:/usr/local/lib:/usr/lib64:/usr/lib:/lib64:/lib
+
+
+cd /public/home/ssjxusr/Asturian/rwkv-rs-optimized-main
 
 cargo run -p rwkv-lm --example rwkv-lm-infer --release --no-default-features --features inferring,cuda,trace -- \
   --config-dir examples/rwkv-lm/config \
